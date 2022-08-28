f37c7ef7516430fd393d08ba32566ddaec135a71
AlineCantalice <alinedecantalice@gmail.com>
2022-08-28 16:06:32 -0300

    recebe url e retorna html da pagina

--- src/pages/QrCodePage.js
+++ src/pages/QrCodePage.js
@@ -5,25 +5,17 @@ export default function QrCodePage() {
     const [urlQrCode, setUrlQrCode] = useState('')
     const [page, setPage] = useState('')
 
-    function sendForm(event) {
+    async function sendForm(event) {
         event.preventDefault()
-        axios.get('/', { link: urlQrCode }).then(res => openHtml(res.data)).catch(err => console.log(err))
-    }
-
-    function openHtml(page) {
-        if (page) {
-            setPage(page)
-            return (
-                <>{page}</>
-            )
-        }
-        return <></>
+        const body = { link: urlQrCode }
+        const data = await axios.post('http://localhost:4000/link', body);
+        setPage(data.data)
     }
 
     return (
         <>
             <form onSubmit={sendForm}>
-                <input type="text" placeholder="entre com a url" value={urlQrCode} name={urlQrCode} onChange={e => setUrlQrCode(e.target.value)} required />
+                <input type="text" placeholder="entre com a url" value={urlQrCode} onChange={e => setUrlQrCode(e.target.value)} required />
                 <button type="submit">Press</button>
             </form>
             {page}
